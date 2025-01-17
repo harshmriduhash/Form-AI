bab5c7c98dceead84dd13ac9ee24b5be12fca1f0
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-17 15:51:16 +0530

    index

--- constant/index.ts
+++ constant/index.ts
@@ -4,19 +4,18 @@ export const defaultBackgroundColor = "#dbf5e1";
 
 export const allBlockLayouts = ["RowLayout"];
 
-
 export const fontWeightClass = {
-    normal: "font-normal",
-    bold: "font-bold",
-    bolder: "font-extrabold",
-    lighter: "font-extralight",
+  normal: "font-normal",
+  bold: "font-bold",
+  bolder: "font-extrabold",
+  lighter: "font-extralight",
 };
 
 export const fontSizeClass = {
-    small: "text-sm",
-    medium: "text-base",
-    large: "text-lg",
-    "x-large": "text-xl",
-    "2x-large": "text-2xl",
-    "4x-large": "text-4xl",
+  small: "text-sm",
+  medium: "text-base",
+  large: "text-lg",
+  "x-large": "text-xl",
+  "2x-large": "text-2xl",
+  "4x-large": "text-4xl",
 };
